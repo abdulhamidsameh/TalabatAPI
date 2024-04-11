8057d28936ea2ceeac8bd04299a5b6f61afcd950
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-04-11 13:49:13 +0200

    Part 08 Product Module - Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class Product : BaseEntity
+	{
+		public string Name { get; set; } = null!;
+		public string Description { get; set; } = null!;
+		public string PictureUrl { get; set; } = null!;
+		public decimal Price { get; set; }
+		
+		public int BrandId { get; set; } // FK : ProductBrand
+		public ProductBrand Brand { get; set; } = null!; // Navigational Property [ONE]
+		
+		public int CategoryId { get; set; } // FK : ProductCategory
+		public ProductCategory Category { get; set; } = null!; // Navigational Property [ONE]
+	}
+}
