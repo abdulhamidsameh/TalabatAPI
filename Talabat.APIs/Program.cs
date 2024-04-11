3ecfb1b231d12c07a2d7bef16bfccf8dcb7971b7
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-04-11 15:02:30 +0200

    Part 10 Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -5,7 +5,7 @@ namespace Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
@@ -15,17 +15,34 @@ namespace Talabat.APIs
 			webApplicationBuilder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webApplicationBuilder.Services.AddEndpointsApiExplorer();
-			webApplicationBuilder.Services.AddSwaggerGen(); 
+			webApplicationBuilder.Services.AddSwaggerGen();
 			webApplicationBuilder.Services.AddDbContext<StoreContext>(options =>
 			{
 				options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
 			});
-			
-			
+
+
 			#endregion
 
 
 			var app = webApplicationBuilder.Build();
+
+			using var scope = app.Services.CreateScope();
+			var services = scope.ServiceProvider;
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			// Ask CLR for Creating Object From DbContext Explicitly
+
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+			try
+			{
+				await _dbContext.Database.MigrateAsync();
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "an error has been occured during apply the migration");
+			}
 
 			#region Configure Kestrel Middlewares
 			// Configure the HTTP request pipeline.
@@ -40,6 +57,8 @@ namespace Talabat.APIs
 			app.MapControllers();
 
 			#endregion
+
+
 
 			app.Run();
 		}
