a79b978418cf92b7bb66eba936059dff461b0c7d
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-04-18 06:35:24 +0200

    Part 11 Server Error (Exception) Handling

---
+++ Talabat.APIs/Errors/ApiExceptionResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Talabat.APIs.Errors
+{
+	public class ApiExceptionResponse : ApiResponse
+	{
+		public string? Description { get; set; }
+		public ApiExceptionResponse(int statusCode , string? message = null , string? description =null )
+			: base(statusCode,message)
+		{
+			Description = description;
+		}
+	}
+}
--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Talabat.APIs.Errors;
 using Talabat.APIs.Helper;
+using Talabat.APIs.Middlewares;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories.Contract;
 using Talabat.Repostiory;
@@ -74,6 +75,9 @@ namespace Talabat.APIs
 			}
 
 			#region Configure Kestrel Middlewares
+
+			app.UseMiddleware<ExceptionMiddleware>();
+
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
