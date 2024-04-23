142c3d2864d4f105a61abbafb0703b55785ffa37
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-04-23 05:37:27 +0200

    Part 02 Basket Module Entities

---
+++ Talabat.Core/Entities/BasketItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class BasketItem
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string PictureUrl { get; set; } = null!;
+		public decimal Price { get; set; }
+		public string Category { get; set; } = null!;
+		public string Brand { get; set; } = null!;
+        public int Quantity { get; set; }
+    }
+}
