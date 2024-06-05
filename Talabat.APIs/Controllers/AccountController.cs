04ad0b678127d06a793e00c4b7d4936769628c2b
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-05 06:52:53 +0300

    Part 04 Register Endpoint

--- Talabat.APIs/Controllers/AccountController.cs
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -13,8 +13,8 @@ namespace Talabat.APIs.Controllers
 		private readonly SignInManager<ApplicationUser> _signInManager;
 
 		public AccountController(UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
-        {
+			SignInManager<ApplicationUser> signInManager)
+		{
 			_userManager = userManager;
 			_signInManager = signInManager;
 		}
@@ -23,7 +23,7 @@ namespace Talabat.APIs.Controllers
 		public async Task<ActionResult<UserDto>> Login(LoginDto model)
 		{
 			var user = await _userManager.FindByEmailAsync(model.Email);
-			if(user is  null) return Unauthorized(new ApiResponse(401,"Invalid Login"));
+			if (user is null) return Unauthorized(new ApiResponse(401, "Invalid Login"));
 
 			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
 
@@ -37,5 +37,27 @@ namespace Talabat.APIs.Controllers
 			});
 
 		}
-    }
+
+		[HttpPost("register")] // POST : /api/Account/register
+		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+		{
+			var user = new ApplicationUser()
+			{
+				DisplayName = model.DisplayName,
+				Email = model.Email,
+				UserName = model.Email.Split('@')[0],
+				PhoneNumber = model.Phone,
+			};
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded) return BadRequest(new ApiValidationErrorResponse() { Errors = result.Errors.Select(E => E.Description) });
+
+			return Ok(new UserDto()
+			{
+				DisplayName = user.DisplayName,
+				Email = user.Email,
+				Token = "this will Be Token",
+			});
+		}
+	}
 }
