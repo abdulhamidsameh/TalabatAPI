04a78a534f0e2f1dfd6afcf3586d99dfc609c747
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-05 11:07:32 +0300

    Part 02 Order Module - Configurartion Classes

---
+++ Talabat.Repostiory/Config/Order Config/OrderConfigrations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OrderAggregate;
+
+namespace Talabat.Repostiory.Config.Order_Config
+{
+	internal class OrderConfigrations : IEntityTypeConfiguration<Order>
+	{
+		public void Configure(EntityTypeBuilder<Order> builder)
+		{
+			builder.OwnsOne(order => order.ShippingAddress, shippingAdress => shippingAdress.WithOwner());
+
+			builder.Property(order => order.Status)
+				.HasConversion(
+				
+				(OStatus) => OStatus.ToString(),
+				(OStatus) => (OrderStatus) Enum.Parse(typeof(OrderStatus),OStatus)
+
+				);
+
+			builder.Property(order => order.SubTotal)
+				.HasColumnType("decimal(12,2)");
+
+		}
+	}
+}
