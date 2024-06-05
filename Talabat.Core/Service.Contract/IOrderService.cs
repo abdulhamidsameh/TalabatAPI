9f541d894ac09745c3ea5906eb42f6a3032f6334
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-05 11:46:04 +0300

    Part 05 Order Service

---
+++ Talabat.Core/Service.Contract/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.OrderAggregate;
+
+namespace Talabat.Core.Service.Contract
+{
+	public interface IOrderService
+	{
+		Task<Order> CreateOrderAsync(string bayerEmail, string basketId, string deliveryMethodId, Address shippingAddress);
+		Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string bayerEmail);
+		Task<Order> GetOrderByIdForUserAsync(string bayerEmail,int orderId);
+		Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+	}
+}
